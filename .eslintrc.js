3080474c6eff0b1aeeb3a61696f59a71d9baa8c9
Arnelle Balane <arnellebalane@gmail.com>
2019-12-30 02:41:42 +0800

    Autofix existing linter violations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,32 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es6': true
+  env: {
+    browser: true,
+    es6: true
   },
-  'extends': [
-    'eslint:recommended',
-    'prettier'
-  ],
-  'plugins': [
-    'prettier'
-  ],
-  'parserOptions': {
-    'ecmaVersion': 2018,
-    'sourceType': 'module'
+  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
   },
-  'rules': {
+  rules: {
     'prettier/prettier': 'error'
-  }
+  },
+  overrides: [
+    {
+      files: ['src/server.js', 'src/routes/api/**/*.js'],
+      env: {
+        node: true
+      }
+    },
+    {
+      files: ['.eslintrc', '*.config.js'],
+      env: {
+        node: true
+      },
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ]
 };
--- src/routes/api/featured-projects.json.js
+++ src/routes/api/featured-projects.json.js
@@ -1,21 +1,25 @@
-export async function get(req, res, next) {
+export async function get(req, res) {
   res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify([
-    {
-      name: 'Streaks',
-      description: 'A Web app for tracking habits using a GitHub-like calendar chart. All data is stored locally on the browser.',
-      tags: ['Vue.js', 'IndexedDB'],
-      url: 'https://streaks.patootie.app/',
-      repo: 'https://github.com/arnellebalane/streaks',
-      image: '/projects/streaks.jpg'
-    },
-    {
-      name: 'Endpoints',
-      description: 'Built the frontend of a Web app for creating URL endpoints with customizable responses, useful as dummy APIs when developing other apps.',
-      tags: ['Vue.js', 'WebSockets'],
-      url: 'https://endpoints.uncaughexception.wtf/',
-      repo: 'https://github.com/uncaughtxcptn/endpoints',
-      image: '/projects/endpoints.jpg'
-    }
-  ]));
+  res.end(
+    JSON.stringify([
+      {
+        name: 'Streaks',
+        description:
+          'A Web app for tracking habits using a GitHub-like calendar chart. All data is stored locally on the browser.',
+        tags: ['Vue.js', 'IndexedDB'],
+        url: 'https://streaks.patootie.app/',
+        repo: 'https://github.com/arnellebalane/streaks',
+        image: '/projects/streaks.jpg'
+      },
+      {
+        name: 'Endpoints',
+        description:
+          'Built the frontend of a Web app for creating URL endpoints with customizable responses, useful as dummy APIs when developing other apps.',
+        tags: ['Vue.js', 'WebSockets'],
+        url: 'https://endpoints.uncaughexception.wtf/',
+        repo: 'https://github.com/uncaughtxcptn/endpoints',
+        image: '/projects/endpoints.jpg'
+      }
+    ])
+  );
 }
