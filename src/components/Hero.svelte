93d292e7fb15a8cd64d2fa5c6d5356517aabfe82
Arnelle Balane <arnellebalane@gmail.com>
2019-12-29 08:06:14 +0800

    Add avatar image to Hero component

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,5 +1,7 @@
-<section class="pt-24">
-  <div class="max-w-lg">
+<section class="flex pt-24">
+  <img src="avatar.jpg" alt="Arnelle Balane" class="w-64 h-80 mr-8 rounded-lg object-cover object-center">
+
+  <div class="max-w-lg my-4">
     <p class="mb-2">👋 Hello! My name is</p>
     <h1 class="text-5xl font-black mb-8">Arnelle Balane</h1>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,10 @@ module.exports = {
       fontFamily: {
         sans: ['"Inter Var"', '"Inter"', ...defaultTheme.fontFamily.sans],
         mono: ['"Roboto Mono"', ...defaultTheme.fontFamily.mono]
+      },
+      height: {
+        72: '18rem',
+        80: '20rem'
       }
     }
   },
