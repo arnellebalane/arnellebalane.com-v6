843aa37d4ffd8d3e73d70fe459574efa9ad4c5f2
Arnelle Balane <arnellebalane@gmail.com>
2020-01-01 15:49:54 +0800

    Make Hero component responsive

--- src/components/HeroSection.svelte
+++ src/components/HeroSection.svelte
@@ -1,9 +1,12 @@
-<section class="flex pt-24 pb-32">
-  <img src="avatar.jpg" alt="Arnelle Balane" class="w-64 h-80 mr-8 rounded-lg object-cover object-center" />
+<section class="flex flex-col-reverse sm:flex-row pt-12 md:pt-24 pb-16 md:pb-32">
+  <img
+    src="avatar.jpg"
+    alt="Arnelle Balane"
+    class="w-32 md:w-64 h-40 md:h-80 mx-auto sm:mx-0 sm:mr-5 md:mr-8 rounded-lg object-cover object-center" />
 
   <div class="max-w-lg my-4">
     <p class="mb-2">👋 Hello! My name is</p>
-    <h1 class="text-5xl font-black mb-8">Arnelle Balane</h1>
+    <h1 class="text-3xl sm:text-4xl md:text-5xl font-black mb-8">Arnelle Balane</h1>
 
     <p class="mb-4">
       I'm a software developer based in Cebu, Philippines focused on building fast and high-quality Web applications.
