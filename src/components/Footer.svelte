eb36eda903a7c0a8fe541a6613b511f313843d49
Arnelle Balane <arnellebalane@gmail.com>
2020-01-04 00:17:36 +0800

    Decrease footer padding and text size on small screens

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,4 +1,4 @@
-<footer class="p-8 font-mono text-center bg-gray-100">
-  <small class="block my-1 text-sm">Designed and built by Arnelle Balane</small>
-  <small class="block my-1 text-sm">Built using Sapper and TailwindCSS</small>
+<footer class="p-6 sm:p-8 font-mono text-center bg-gray-900 text-white">
+  <small class="block my-1 text-xs sm:text-sm">Designed and built by Arnelle Balane</small>
+  <small class="block my-1 text-xs sm:text-sm">Built using Sapper and TailwindCSS</small>
 </footer>
