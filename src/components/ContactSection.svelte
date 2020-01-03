7c3cb1120674bb7a1e6c5f146f9b524f35327b5b
Arnelle Balane <arnellebalane@gmail.com>
2020-01-03 18:29:03 +0800

    Render ContactSection component

---
+++ src/components/ContactSection.svelte
@@ -0,0 +1,11 @@
+<div class="pt-40 pb-64">
+  <h1 class="text-2xl sm:text-3xl font-black text-center mb-8">Get In Touch</h1>
+
+  <p class="max-w-lg mx-auto mb-6 text-center">
+    Although I'm currently not available for hire, my inbox is always open for possible collaboration or just to say hi.
+    <a href="mailto:arnellebalane@gmail.com" class="text-primary">Send me an email</a>
+    and I will do my best to reply!
+  </p>
+
+  <p class="max-w-lg mx-auto mb-6 text-center">Have a great day! 🎉</p>
+</div>
