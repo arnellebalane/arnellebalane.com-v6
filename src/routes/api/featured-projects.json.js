d1363ce0e3c34e186423ce7dd50f446eec11b6c5
Arnelle Balane <arnellebalane@gmail.com>
2020-01-04 04:34:48 +0800

    Fetch featured projects data from Contentful Delivery API

--- src/routes/api/featured-projects.json.js
+++ src/routes/api/featured-projects.json.js
@@ -1,25 +1,19 @@
+import contentful from '../_lib/contentful';
+
 export async function get(req, res) {
+  const entries = await contentful.getEntries({
+    content_type: 'project',
+    'fields.is_featured': true
+  });
+  const data = entries.items.map(({ fields }) => ({
+    name: fields.name,
+    description: fields.description,
+    url: fields.url,
+    repo: fields.repository,
+    tags: fields.tags,
+    image: fields.image.fields.file.url
+  }));
+
   res.setHeader('Content-Type', 'application/json');
-  res.end(
-    JSON.stringify([
-      {
-        name: 'Streaks',
-        description:
-          'A Web app for tracking habits using a GitHub-like calendar chart. All data is stored locally on the browser.',
-        tags: ['Vue.js', 'IndexedDB'],
-        url: 'https://streaks.patootie.app/',
-        repo: 'https://github.com/arnellebalane/streaks',
-        image: '/projects/streaks.jpg'
-      },
-      {
-        name: 'Endpoints',
-        description:
-          'Built the frontend of a Web app for creating URL endpoints with customizable responses, useful as dummy APIs when developing other apps.',
-        tags: ['Vue.js', 'WebSockets'],
-        url: 'https://endpoints.uncaughexception.wtf/',
-        repo: 'https://github.com/uncaughtxcptn/endpoints',
-        image: '/projects/endpoints.jpg'
-      }
-    ])
-  );
+  res.end(JSON.stringify(data));
 }
