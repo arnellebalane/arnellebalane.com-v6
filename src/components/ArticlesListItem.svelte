f91911781f5304c3089cfd6ffbadb22838f67a64
Arnelle Balane <arnellebalane@gmail.com>
2020-01-04 21:54:21 +0800

    Ensure same aspect ratio for article images

--- src/components/ArticlesListItem.svelte
+++ src/components/ArticlesListItem.svelte
@@ -33,27 +33,33 @@
     box-shadow: 0 0 1rem rgba(0, 0, 0, 0.05);
   }
 
-  img {
+  .image-wrapper {
     width: calc(100% + 2.5rem); /* 2.5rem = 2 * .mx-5 */
+  }
+
+  .image-wrapper::before {
+    content: '';
+    display: block;
+    padding-top: calc(100% / 16 * 9);
   }
 
   /* Responsive breakpoint based on Tailwind's "sm" and "xl" breakpoints. */
   @media (min-width: 640px) and (max-width: 1279px) {
-    img {
+    .image-wrapper {
       width: calc(100% + 5rem); /* 5rem = 2 * .mx-10 */
     }
   }
 
   /* Responsive breakpoint based on Tailwind's "xl" breakpoint. */
   @media (max-width: 1279px) {
-    img {
+    .image-wrapper {
       max-width: initial;
     }
   }
 
   /* Responsive breakpoint based on Tailwind's "xl" breakpoint. */
   @media (min-width: 1280px) {
-    img {
+    .image-wrapper {
       top: 50%;
       transform: translateY(-50%);
     }
@@ -63,15 +69,19 @@
 <article class="relative p-5 mb-6 sm:p-10 sm:mb-12 border border-solid border-gray-100 rounded-lg xl:pr-56 xl:mr-48">
   <header class="mb-4 sm:mb-8">
     {#if article.cover_image}
-      <img
-        src={`${article.cover_image}?fm=webp&q=80&w=400`}
-        srcset={getImageSrcset(article.cover_image)}
-        sizes="(min-width: 1280px) 400px, (min-width: 768px) 80vw, 100vw"
-        alt={article.title}
-        loading="lazy"
-        class="xl:w-full -m-5 mb-5 sm:-m-10 sm:mb-10 rounded-t-lg xl:rounded-b-lg xl:absolute xl:right-0 xl:m-0
-        xl:-mr-48 xl:max-w-sm xl:shadow-xl"
-      />
+      <div
+        class="image-wrapper xl:w-full -m-5 mb-5 sm:-m-10 sm:mb-10 relative xl:absolute xl:right-0 xl:m-0 xl:-mr-48
+        xl:max-w-sm xl:shadow-xl"
+      >
+        <img
+          src={`${article.cover_image}?fm=webp&q=80&w=400`}
+          srcset={getImageSrcset(article.cover_image)}
+          sizes="(min-width: 1280px) 400px, (min-width: 768px) 80vw, 100vw"
+          alt={article.title}
+          loading="lazy"
+          class="absolute inset-0 w-full h-full rounded-t-lg xl:rounded-b-lg object-cover object-center"
+        />
+      </div>
     {/if}
 
     <a href={article.url} target={article.source ? '_blank' : ''} rel={article.source ? 'noopener noreferrer' : ''}>
