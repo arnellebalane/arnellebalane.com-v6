63ac7c081b36bbcb02ee4069a3da144346f70f39
Arnelle Balane <arnellebalane@gmail.com>
2020-01-16 00:02:39 +0800

    Redesign articles list page

--- src/components/ArticlesListItem.svelte
+++ src/components/ArticlesListItem.svelte
@@ -17,93 +17,30 @@
         return '';
     }
   }
-
-  function getImageSrcset(image) {
-    return [
-      `${image}?fm=webp&q=80&w=400 400w`,
-      `${image}?fm=webp&q=80&w=560 560w`,
-      `${image}?fm=webp&q=80&w=730 730w`,
-      `${image}?fm=webp&q=80&w=840 840w`
-    ].join(',');
-  }
 </script>
 
-<style>
-  article {
-    box-shadow: 0 0 1rem rgba(0, 0, 0, 0.05);
-  }
+<article class="mb-24 sm:mb-32">
+  <div class="max-w-2xl">
+    <header class="mb-4 sm:mb-8">
+      <a href={article.url} target={article.source ? '_blank' : ''} rel={article.source ? 'noopener noreferrer' : ''}>
+        <h1 class="text-2xl sm:text-3xl font-bold leading-snug">{article.title}</h1>
+      </a>
+    </header>
 
-  .image-wrapper {
-    width: calc(100% + 2.5rem); /* 2.5rem = 2 * .mx-5 */
-  }
+    <p>{article.description}</p>
 
-  .image-wrapper::before {
-    content: '';
-    display: block;
-    padding-top: calc(100% / 16 * 9);
-  }
-
-  /* Responsive breakpoint based on Tailwind's "sm" and "xl" breakpoints. */
-  @media (min-width: 640px) and (max-width: 1279px) {
-    .image-wrapper {
-      width: calc(100% + 5rem); /* 5rem = 2 * .mx-10 */
-    }
-  }
-
-  /* Responsive breakpoint based on Tailwind's "xl" breakpoint. */
-  @media (max-width: 1279px) {
-    .image-wrapper {
-      max-width: initial;
-    }
-  }
-
-  /* Responsive breakpoint based on Tailwind's "xl" breakpoint. */
-  @media (min-width: 1280px) {
-    .image-wrapper {
-      top: 50%;
-      transform: translateY(-50%);
-    }
-  }
-</style>
-
-<article class="relative p-5 mb-6 sm:p-10 sm:mb-12 border border-solid border-gray-100 rounded-lg xl:pr-56 xl:mr-48">
-  <header class="mb-4 sm:mb-8">
-    {#if article.cover_image}
-      <div
-        class="image-wrapper xl:w-full -m-5 mb-5 sm:-m-10 sm:mb-10 relative xl:absolute xl:right-0 xl:m-0 xl:-mr-48
-        xl:max-w-sm xl:shadow-xl"
+    <footer class="flex flex-wrap mt-4 sm:mt-8 -mx-4 font-mono text-base text-gray-600">
+      <time datetime={article.date} class="mx-4">{formatDate(article.date)}</time>
+      <a
+        href={article.url}
+        class="mx-4"
+        class:text-medium-green={article.source === 'medium'}
+        class:text-devto-purple={article.source === 'devto'}
+        target={article.source ? '_blank' : ''}
+        rel={article.source ? 'noopener noreferrer' : ''}
       >
-        <img
-          src={`${article.cover_image}?fm=webp&q=80&w=400`}
-          srcset={getImageSrcset(article.cover_image)}
-          sizes="(min-width: 1280px) 400px, (min-width: 768px) 80vw, 100vw"
-          alt={article.title}
-          loading="lazy"
-          width="160px"
-          height="90px"
-          class="absolute inset-0 w-full h-full rounded-t-lg xl:rounded-b-lg object-cover object-center"
-        />
-      </div>
-    {/if}
-
-    <a href={article.url} target={article.source ? '_blank' : ''} rel={article.source ? 'noopener noreferrer' : ''}>
-      <h1 class="text-2xl sm:text-3xl font-bold leading-snug">{article.title}</h1>
-    </a>
-  </header>
-
-  <p>{article.description}</p>
-
-  <footer class="flex flex-wrap mt-6 sm:mt-12 -mx-4 font-mono text-base text-gray-600">
-    <time datetime={article.date} class="mx-4">{formatDate(article.date)}</time>
-    <a
-      href={article.url}
-      class="mx-4"
-      class:text-medium-green={article.source === 'medium'}
-      class:text-devto-purple={article.source === 'devto'}
-      target={article.source ? '_blank' : ''}
-      rel={article.source ? 'noopener noreferrer' : ''}
-    >
-      Read more {formatSource(article.source)}
-    </a>
-  </footer>
+        Read more {formatSource(article.source)}
+      </a>
+    </footer>
+  </div>
 </article>
