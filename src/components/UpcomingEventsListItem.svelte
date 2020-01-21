1cf488d228718da0846ca187edff4f3c872e3fb0
Arnelle Balane <arnellebalane@gmail.com>
2020-01-22 02:08:46 +0800

    Display UpcomingEventsListItem for each upcoming event data

---
+++ src/components/UpcomingEventsListItem.svelte
@@ -0,0 +1,29 @@
+<script>
+  import dayjs from 'dayjs';
+
+  export let event;
+
+  function formatDate(date) {
+    return dayjs(date).format('MMMM D, YYYY');
+  }
+</script>
+
+<article class="py-10 mb-12">
+  <a href={event.url} target="_blank" rel="noopener noreferrer">
+    <h1 class="mb-6 text-2xl sm:text-3xl font-bold leading-snug">{event.name}</h1>
+  </a>
+
+  <small
+    class="block font-mono text-sm text-gray-700 uppercase"
+    class:text-pink-700={event.type === 'talk'}
+    class:text-orange-700={event.type === 'workshop'}
+  >
+    {event.type}
+  </small>
+  <h2 class="text-lg sm:text-xl font-normal">{event.session}</h2>
+
+  <footer class="mt-8 font-mono text-base text-gray-700">
+    <time datetime={event.date}>{formatDate(event.date)}</time>
+    <p>{event.location}</p>
+  </footer>
+</article>
