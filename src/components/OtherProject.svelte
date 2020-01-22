261aa561d4c6ef240b1d555dc0504a108d8831f6
Arnelle Balane <arnellebalane@gmail.com>
2020-01-22 23:16:39 +0800

    Use OtherProject name text as hyperlink to repo

--- src/components/OtherProject.svelte
+++ src/components/OtherProject.svelte
@@ -2,20 +2,13 @@
   export let project;
 </script>
 
-<style>
-  li:first-child a::before {
-    content: '> ';
-  }
-</style>
-
 <div class="flex flex-col px-6 py-5 bg-gray-100 rounded">
-  <h1 class="text-xl font-black mb-2">{project.name}</h1>
+  <a href={project.repo} target="_blank" rel="noopener noreferrer" class="mb-2">
+    <h1 class="text-xl font-black">{project.name}</h1>
+  </a>
   <p class="text-base mb-6">{project.description}</p>
 
   <ul class="flex flex-wrap -mx-3 mt-auto">
-    <li class="mx-3 font-mono text-xs text-primary uppercase">
-      <a href={project.repo} target="_blank" rel="noopener noreferrer">View</a>
-    </li>
     {#each project.tags as tag}
       <li class="mx-3 font-mono text-xs text-gray-700">{tag}</li>
     {/each}
