579bbb13b08b9efe06887c53c1c1802d68118ef3
Arnelle Balane <arnellebalane@gmail.com>
2020-01-26 17:23:59 +0800

    Add heading and subtitle to projects page

---
+++ src/routes/projects.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let projects = [];
+</script>
+
+<svelte:head>
+  <title>Projects | Arnelle Balane</title>
+</svelte:head>
+
+<h1 class="text-2xl sm:text-3xl font-black mb-2 sm:mb-4">Projects</h1>
+<p>A list of things that I've built and worked on</p>
