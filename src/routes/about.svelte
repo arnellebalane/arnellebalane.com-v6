96270a2ede3a6bdb02cf465baf1b64ca7cb45481
Arnelle Balane <arnellebalane@gmail.com>
2020-01-30 01:32:38 +0800

    Add initial content for about page

---
+++ src/routes/about.svelte
@@ -0,0 +1,35 @@
+<svelte:head>
+  <title>About | Arnelle Balane</title>
+</svelte:head>
+
+<div class="flex justify-between items-start">
+  <div class="max-w-xl mb-32">
+    <h1 class="mb-6 text-2xl sm:text-3xl font-black">About Me</h1>
+
+    <p class="mb-6">
+      Hello! I’m Arnelle, a software developer based in Cebu, Philippines. I enjoy building things using good old HTML,
+      CSS, JavaScript, and Web Platform APIs. I develop performant Web applications that provide good user experiences.
+    </p>
+    <p class="mb-6">
+      I'm a Google Developers Expert in Web Technologies, and an active volunteer and speaker for local tech
+      communities, giving talks and conducting workshops about various technical topics.
+    </p>
+    <p class="mb-6">
+      When not busy experimenting with new Web APIs, I like to spend my time taking photos, playing video games, or
+      practicing my card tricks.
+    </p>
+  </div>
+
+  <img src="avatar.jpg" alt="Arnelle Balane" class="hidden sm:block w-1/4 lg:w-1/3 mt-10 ml-10 rounded" />
+</div>
+
+<div class="max-w-xl mb-32">
+  <h2 id="speaker-profile" class="mb-6 text-xl sm:text-2xl font-black">Speaker Profile</h2>
+
+  <p>
+    Arnelle is a Web developer from Cebu, Philippines and a Google Developers Expert for Web Technologies who enjoys
+    building things with good old HTML, CSS, JavaScript, and Web Platform APIs. He is an active volunteer and speaker
+    for GDG Cebu and other local JavaScript communities. When not busy experimenting with new Web APIs, he can also be
+    seen taking photos, playing video games, or practicing his card tricks.
+  </p>
+</div>
