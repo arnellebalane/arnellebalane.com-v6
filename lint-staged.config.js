30c846ef8142736636d18310136ecfc490f6d3e6
Arnelle Balane <arnellebalane@gmail.com>
2020-02-07 02:02:28 +0800

    Use lint-staged config workaround for issue with brackets in filenames

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,18 @@
+const { quote } = require('shell-quote');
+
 module.exports = {
-  '*.{js,svelte}': ['eslint --fix', 'prettier --write', 'git add']
+  // TODO: This is a workaround for a known bug in lint-staged where some
+  // commands can't handle filenames with brackets in them.
+  // https://github.com/okonet/lint-staged/issues/676#issuecomment-518850249
+  '*.{js,svelte}': filenames => {
+    const commands = [];
+    filenames.forEach(filename => {
+      commands.push(
+        `eslint --fix ${filename}`,
+        quote(['prettier', '--write', filename]),
+        quote(['git', 'add', filename])
+      );
+    });
+    return commands;
+  }
 };
