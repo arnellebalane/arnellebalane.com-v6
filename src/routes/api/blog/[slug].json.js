73899a37017c8cf5c3be6380183c0c7a7cd7b9b2
Arnelle Balane <arnellebalane@gmail.com>
2020-02-07 02:17:40 +0800

    Return parsed article data including its frontmatter from article API

--- src/routes/api/blog/[slug].json.js
+++ src/routes/api/blog/[slug].json.js
@@ -1,14 +1,22 @@
 import path from 'path';
 import { promises as fs } from 'fs';
+import pick from 'lodash/pick';
+import frontmatter from 'front-matter';
+import marked from 'marked';
 
 // process.cwd() will _usually_ point to the project's root directory.
-// This seems very fragile though, hoping to improve this soon.
+// TODO: This seems very fragile though, hoping to improve this soon.
 const articlesDir = path.join(process.cwd(), 'src/routes/_articles');
 
 export async function get(req, res) {
   const { slug } = req.params;
-  const markdown = await fs.readFile(path.join(articlesDir, `${slug}.md`), 'utf8');
+  const content = await fs.readFile(path.join(articlesDir, `${slug}.md`), 'utf8');
+  const extracted = frontmatter(content);
+  const response = {
+    meta: pick(extracted.attributes, ['title', 'description', 'image', 'categories', 'tags', 'date']),
+    html: marked(extracted.body)
+  };
 
   res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ html: markdown }));
+  res.end(JSON.stringify(response));
 }
