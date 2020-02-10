74aa4b1442f74118ff1ec0fb3811dbc960c4d017
Arnelle Balane <arnellebalane@gmail.com>
2020-02-11 00:41:39 +0800

    Display article title and set max-width to 70 chars

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -13,6 +13,14 @@
   export let article;
 </script>
 
-<div>
+<style>
+  div {
+    max-width: 70ch;
+  }
+</style>
+
+<div class="mx-auto">
+  <h1 class="mb-8 sm:mb-16 text-3xl sm:text-4xl font-black">{article.meta.title}</h1>
+
   {@html article.html}
 </div>
