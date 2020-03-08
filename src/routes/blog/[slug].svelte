782a15cffa68400c1a43766ee663b5df0cf57f67
Arnelle Balane <arnellebalane@gmail.com>
2020-03-08 19:56:21 +0800

    Display article date and enable share functionality

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -10,7 +10,31 @@
 </script>
 
 <script>
+  import dayjs from 'dayjs';
+
   export let article;
+
+  const postedOn = dayjs(article.meta.date).format('MMMM DD, YYYY');
+  const url = location.origin + location.pathname;
+
+  function tweetArticle() {
+    const tweetText = `"${document.title}" by @arnellebalane: ${url}`;
+    const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}`;
+    window.open(tweetUrl, 'new-tweet');
+  }
+
+  function shareArticle() {
+    const UNSUPPORTED_WEB_SHARE_ERROR = 'Internal error: could not connect to Web Share interface.';
+
+    navigator.share({ text: document.title, title: document.title, url }).catch(error => {
+      if (error.message === UNSUPPORTED_WEB_SHARE_ERROR) {
+        tweetArticle();
+      }
+    });
+  }
+
+  const shareLabel = navigator.share ? 'Share this article' : 'Tweet this article';
+  const shareHandler = navigator.share ? shareArticle : tweetArticle;
 </script>
 
 <style>
@@ -19,8 +43,18 @@
   }
 </style>
 
+<svelte:head>
+  <title>{article.meta.title}</title>
+</svelte:head>
+
 <div class="article mx-auto">
-  <h1 class="mb-8 sm:mb-16 text-3xl sm:text-4xl font-black leading-normal">{article.meta.title}</h1>
+  <header class="mb-24 sm:mb-16">
+    <h1 class="mb-4 text-3xl sm:text-4xl font-black leading-normal">{article.meta.title}</h1>
+    <div>
+      <time class="mr-12 text-gray-700" datetime="article.date">{postedOn}</time>
+      <button class="underline text-primary" on:click={shareHandler}>{shareLabel}</button>
+    </div>
+  </header>
 
   {@html article.html}
 </div>
