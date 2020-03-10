1a675e508b3f7fb1a2246c03054106ffc52be3ea
Arnelle Balane <arnellebalane@gmail.com>
2020-03-10 17:11:24 +0800

    Use absolute URLs for article cover images

--- src/routes/api/articles.json.js
+++ src/routes/api/articles.json.js
@@ -4,6 +4,8 @@ import sortBy from 'lodash/sortBy';
 import contentful from '@lib/contentful';
 import getArticles from '@lib/get-articles';
 
+const baseUrl = process.env.BASE_URL.replace(/\/$/, '');
+
 export async function get(req, res) {
   const [remoteEntries, localEntries] = await Promise.all([
     contentful.getEntries({
@@ -18,7 +20,10 @@ export async function get(req, res) {
       ...pick(fields, ['title', 'description', 'date', 'url', 'source', 'categories']),
       cover_image: _get(fields, 'cover_image.fields.file.url')
     })),
-    localEntries.map(entry => entry.meta)
+    localEntries.map(entry => ({
+      ...entry.meta,
+      cover_image: baseUrl + entry.meta.cover_image
+    }))
   );
 
   // Lodash's sortBy always sorts in ascending order, so we take the negative
