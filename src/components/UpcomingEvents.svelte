68dcb14ac282e66c407f4240206a3c90d22ef20d
Arnelle Balane <arnellebalane@gmail.com>
2020-03-14 16:13:19 +0800

    Add empty state message for upcoming events section

--- src/components/UpcomingEvents.svelte
+++ src/components/UpcomingEvents.svelte
@@ -2,8 +2,20 @@
   import UpcomingEventsItem from './UpcomingEventsItem.svelte';
 
   export let events = [];
+
+  const hasEvents = events.length > 0;
 </script>
 
 {#each events as event}
   <UpcomingEventsItem {event} />
 {/each}
+
+{#if !hasEvents}
+  <p class="max-w-2xl">
+    <strong class="font-black">No upcoming events, for now.</strong>
+    I give talks and workshops about JavaScript and Web topics - Performance, Progressive Web Apps, Project Fugu,
+    Firebase, and others. You can
+    <a class="text-primary font-bold" href="mailto:arnellebalane@gmail.com">send me an email</a>
+    if you want me to share about those topics in your event. Cheers!
+  </p>
+{/if}
