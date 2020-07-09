46fbdaf0651027ee99a740721fe0bd66f85144c9
Arnelle Balane <arnellebalane@gmail.com>
2020-07-09 23:26:26 +0800

    Fix duplicate key linter issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,21 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    serviceworker: true
+    serviceworker: true,
   },
   extends: ['eslint:recommended', 'airbnb-base', 'prettier'],
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   rules: {
     'no-console': ['error', { allow: ['warn', 'error'] }],
     'no-restricted-globals': 'off',
     'import/no-extraneous-dependencies': 'off',
-    'import/no-extraneous-dependencies': 'off',
     'import/no-mutable-exports': 'off',
     'import/no-unresolved': 'off',
     'import/prefer-default-export': 'off',
-    'import/first': 'off'
+    'import/first': 'off',
   },
   overrides: [
     {
@@ -25,26 +24,26 @@ module.exports = {
       processor: 'svelte3/svelte3',
       plugins: ['svelte3'],
       globals: {
-        process: true
-      }
+        process: true,
+      },
     },
     {
       files: ['src/server.js', 'src/routes/api/**/*.js'],
       env: {
-        node: true
-      }
+        node: true,
+      },
     },
     {
       files: ['.eslintrc', '*.config.js'],
       env: {
-        node: true
+        node: true,
       },
       parserOptions: {
-        sourceType: 'script'
+        sourceType: 'script',
       },
       rules: {
-        'global-require': 'off'
-      }
-    }
-  ]
+        'global-require': 'off',
+      },
+    },
+  ],
 };
